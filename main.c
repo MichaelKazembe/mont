cd1fc92e1f0b802a9d84196af0afc52affb4be2e
Michael Kazembe <122834780+MichaelKazembe@users.noreply.github.com>
2023-05-25 00:41:37 +0200

    Update main.c

--- main.c
+++ main.c
@@ -1,8 +1,4 @@
 #include "monty.h"
-
-/* Global variable */
-stack_t *stack = NULL;
-
 /**
  * main - Entry point for the Monty program
  * @argc: The number of arguments passed to the program
@@ -10,6 +6,7 @@ stack_t *stack = NULL;
  *
  * Return: EXIT_SUCCESS on success, or EXIT_FAILURE on failure
  */
+
 int main(int argc, char **argv)
 {
     FILE *file;
@@ -60,48 +57,4 @@ int main(int argc, char **argv)
     fclose(file);
     free_all(&stack);
     return EXIT_SUCCESS;
-}
-
-/**
- * free_all - Frees all nodes in a stack
- * @stack: The stack to free
- */
-void free_all(stack_t **stack)
-{
-    stack_t *current, *next;
-
-    if (stack == NULL || *stack == NULL)
-        return;
-
-    current = *stack;
-    while (current != NULL)
-    {
-        next = current->next;
-        free(current);
-        current = next;
-    }
-    *stack = NULL;
-}
-
-/**
- * get_instruction - Finds the instruction corresponding to a given opcode
- * @opcode: The opcode to search for
- *
- * Return: The instruction corresponding to the opcode, or a null instruction
- * if no corresponding instruction was found.
- */
-instruction_t get_instruction(char *opcode)
-{
-    int i;
-    instruction_t null_instruction = {"", NULL};
-
-    if (opcode == NULL)
-        return null_instruction;
-
-    for (i = 0; instructions[i].opcode != NULL; i++)
-    {
-        if (strcmp(instructions[i].opcode, opcode) == 0)
-            return instructions[i];
-    }
-    return null_instruction;
 }
