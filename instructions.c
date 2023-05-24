d5ed0c934e3cbc0011d633758ca378504ed3a70a
MichaelKazembe <michaelkazembe@gmail.com>
2023-05-24 16:54:31 -0700

    update main.c

--- instructions.c
+++
@@ -1,9 +0,0 @@
-#include "monty.h"
-
-instruction_t instructions[] = {
-    {"push", push_func},
-    {"pall", pall_func},
-    {"pint", pint_func},
-    /* ... other instructions ... */
-    {NULL, NULL}
-};
