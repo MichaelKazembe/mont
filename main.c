ab6efd902bbfcc51335b8f034b597311cd0d3de1
Michael Kazembe <122834780+MichaelKazembe@users.noreply.github.com>
2023-05-25 00:30:01 +0200

    Update main.c

--- main.c
+++ main.c
@@ -1,6 +1,4 @@
 #include "monty.h"
-#include <stdio.h>
-#include <stdlib.h>
 
 /* Global variable */
 stack_t *stack = NULL;
@@ -14,54 +12,54 @@ stack_t *stack = NULL;
  */
 int main(int argc, char **argv)
 {
-        FILE *file;
-        char *line = NULL, *opcode, *arg = NULL;
-        size_t len = 0;
-        unsigned int line_number = 0;
-        instruction_t instruction;
+    FILE *file;
+    char *line = NULL, *opcode, *arg = NULL;
+    size_t len = 0;
+    unsigned int line_number = 0;
+    instruction_t instruction;
 
-        if (argc != 2)
+    if (argc != 2)
+    {
+        fprintf(stderr, "USAGE: monty file\n");
+        return (EXIT_FAILURE);
+    }
+
+    file = fopen(argv[1], "r");
+    if (file == NULL)
+    {
+        fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
+        return (EXIT_FAILURE);
+    }
+
+    while (getline(&line, &len, file) != -1)
+    {
+        line_number++;
+
+        /* Parse the opcode and optional argument */
+        opcode = strtok(line, " \t\n");
+        if (opcode == NULL || opcode[0] == '#')
+            continue;
+        arg = strtok(NULL, " \t\n");
+
+        /* Search for the opcode in the instruction set */
+        instruction = get_instruction(opcode);
+        if (instruction.f == NULL)
         {
-                fprintf(stderr, "USAGE: monty file\n");
-                return (EXIT_FAILURE);
+            fprintf(stderr, "L%d: unknown instruction %s\n", line_number, opcode);
+            free_all(&stack);
+            free(line);
+            fclose(file);
+            return (EXIT_FAILURE);
         }
 
-        file = fopen(argv[1], "r");
-        if (!file)
-        {
-                fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
-                return (EXIT_FAILURE);
-        }
+        /* Execute the found instruction */
+        instruction.f(&stack, line_number, arg);
+    }
 
-        while (getline(&line, &len, file) != -1)
-        {
-                line_number++;
-
-                /* Parse the opcode and optional argument */
-                opcode = strtok(line, " \t\n");
-                if (opcode == NULL || opcode[0] == '#')
-                        continue;
-                arg = strtok(NULL, " \t\n");
-
-                /* Search for the opcode in the instruction set */
-                instruction = get_instruction(opcode);
-                if (instruction.f == NULL)
-                {
-                        fprintf(stderr, "L%d: unknown instruction %s\n", line_number, opcode);
-                        free_all(&stack);
-                        free(line);
-                        fclose(file);
-                        return (EXIT_FAILURE);
-                }
-
-                /* Execute the found instruction */
-                instruction.f(&stack, line_number, arg);
-        }
-
-        free(line);
-        fclose(file);
-        free_all(&stack);
-        return (EXIT_SUCCESS);
+    free(line);
+    fclose(file);
+    free_all(&stack);
+    return (EXIT_SUCCESS);
 }
 
 /**
@@ -70,19 +68,19 @@ int main(int argc, char **argv)
  */
 void free_all(stack_t **stack)
 {
-        stack_t *current, *next;
+    stack_t *current, *next;
 
-        if (stack == NULL || *stack == NULL)
-                return;
+    if (stack == NULL || *stack == NULL)
+        return;
 
-        current = *stack;
-        while (current != NULL)
-        {
-                next = current->next;
-                free(current);
-                current = next;
-        }
-        *stack = NULL;
+    current = *stack;
+    while (current != NULL)
+    {
+        next = current->next;
+        free(current);
+        current = next;
+    }
+    *stack = NULL;
 }
 
 /**
@@ -94,16 +92,16 @@ void free_all(stack_t **stack)
  */
 instruction_t get_instruction(char *opcode)
 {
-        int i;
-        instruction_t null_instruction = {"", NULL};
+    int i;
+    instruction_t null_instruction = {"", NULL};
 
-        if (opcode == NULL)
-                return (null_instruction);
-
-        for (i = 0; instructions[i].opcode; i++)
-        {
-                if (strcmp(instructions[i].opcode, opcode) == 0)
-                        return (instructions[i]);
-        }
+    if (opcode == NULL)
         return (null_instruction);
+
+    for (i = 0; instructions[i].opcode != NULL; i++)
+    {
+        if (strcmp(instructions[i].opcode, opcode) == 0)
+            return instructions[i];
+    }
+    return (null_instruction);
 }
