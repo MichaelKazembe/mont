d757b934da513902c6a806350630edbe3dcf9301
Michael Kazembe <122834780+MichaelKazembe@users.noreply.github.com>
2023-05-25 01:24:45 +0200

    Update is_digit.c

--- is_digit.c
+++ is_digit.c
@@ -1,27 +1,28 @@
 #include "monty.h"
 
 /**
- * is_integer - Checks if a string represents a valid number
- * @str: The string to check
- * Return: true if the string is a valid number, false otherwise
+ * is_number - Checks if a string represents a valid number.
+ * @str: The string to check.
+ * Return: 1 if the string is a valid number, 0 otherwise.
  */
-bool is_integer(const char *str)
+int is_number(char *str)
 {
-     int i = 0;
-     bool has_digits = false;
-    
     if (str == NULL || *str == '\0')
-        return false;
+        return 0;
+
+    int i = 0;
 
     if (str[0] == '-' || str[0] == '+')
         i++;
 
+    int has_digits = 0;
+
     for (; str[i] != '\0'; i++)
     {
-        if (str[i] < '0' || str[i] > '9')
-            return false;
-        
-        has_digits = true;
+        if (!isdigit(str[i]))
+            return 0;
+
+        has_digits = 1;
     }
 
     return has_digits;
