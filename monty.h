f66219e83ee56d23aede5e174aadede2146b7097
Michael Kazembe <122834780+MichaelKazembe@users.noreply.github.com>
2023-05-24 23:32:10 +0200

    Update monty.h

--- monty.h
+++ monty.h
@@ -9,35 +9,28 @@
 #include <string.h>
 #include <ctype.h>
 
+/* Data structures */
+
 /**
- * struct stack_s - doubly linked list representation of a stack (or queue)
- * @n: integer
- * @prev: points to the previous element of the stack (or queue)
- * @next: points to the next element of the stack (or queue)
- *
- * Description: doubly linked list node structure
- * for stack, queues, LIFO, FIFO
+ * struct stack_s - Doubly linked list representation of a stack (or queue)
+ * @n: Integer value stored in the node
+ * @prev: Pointer to the previous element of the stack (or queue)
+ * @next: Pointer to the next element of the stack (or queue)
  */
 typedef struct stack_s
 {
-        int n;
-        struct stack_s *prev;
-        struct stack_s *next;
+    int n;
+    struct stack_s *prev;
+    struct stack_s *next;
 } stack_t;
 
-/**
- * struct instruction_s - opcode and its function
- * @opcode: the opcode
- * @f: function to handle the opcode
- *
- * Description: opcode and its function
- * for stack, queues, LIFO, FIFO
- */
-typedef struct instruction_s
-{
-        char *opcode;
-        void (*f)(stack_t **stack, unsigned int line_number);
-} instruction_t;
+/* Function prototypes */
 
+void push(stack_t **stack, unsigned int line_number);
+void pall(stack_t **stack);
 
-#endif
+int is_integer(char *str);
+stack_t *add_node(stack_t **stack, int n);
+void free_all(stack_t **stack);
+
+#endif /* _MONTY_H */
