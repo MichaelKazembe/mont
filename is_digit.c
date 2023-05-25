78041c601881dc19ebd0562a5dae7fe3565ee41b
Michael Kazembe <122834780+MichaelKazembe@users.noreply.github.com>
2023-05-25 02:04:57 +0200

    betty coding style

--- is_digit.c
+++ is_digit.c
@@ -9,19 +9,20 @@ int is_integer(char *str)
 {
     int i = 0;
     int has_digits = 0;
+    
     if (str == NULL || *str == '\0')
         return 0;
-
+    
     if (str[0] == '-' || str[0] == '+')
         i++;
-
+    
     for (; str[i] != '\0'; i++)
     {
         if (!isdigit(str[i]))
             return 0;
-
+        
         has_digits = 1;
     }
 
-    return has_digits;
+    return (has_digits);
 }
