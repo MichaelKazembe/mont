5b4b042834058c70e073b77c5a8f5739fc95ba50
Michael Kazembe <122834780+MichaelKazembe@users.noreply.github.com>
2023-05-26 02:45:32 +0200

    Update monty.h

--- monty.h
+++ monty.h
@@ -72,6 +72,8 @@ void swap_(stack_t **stack, unsigned int line_number);
 void nop_(stack_t **stack, unsigned int line_number);
 void rotl_(stack_t **stack, unsigned int line_number);
 void rotr_(stack_t **stack, unsigned int line_number);
+void f_queue(stack_t **head, unsigned int counter);
+void f_stack(stack_t **head, unsigned int counter);
 
 /*string ASCII functions*/
 void pchar_(stack_t **stack, unsigned int line_number);
